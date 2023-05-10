d68ad8128df8bba719e7ef0674fd823b241500ed
DeepjyotiMisra <107135550+DeepjyotiMisra@users.noreply.github.com>
2023-05-10 09:14:18 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, Nike Team Members");
+      System.out.println("This is mindshare session");
    }
 }
